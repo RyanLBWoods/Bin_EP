a2e0977f3b967918b9dbc3a8a280b2a621474d89
Lin Bin <linbin@8afbcfa1.cs.st-andrews.ac.uk>
2018-03-14 23:11:33 +0000

    remove code

--- LinearReg.py
+++ LinearReg.py
@@ -1,28 +1,35 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
 
 
 # Absolute Error
-def ae(y_test, y_predict):
-    return abs(y_test - y_predict)
+def ae(test, predict):
+    return abs(test - predict)
 
 
 # Mean Absolute Error
-def mae(y_test, y_predict):
-    return ae(y_test, y_predict).mean()
+def mae(test, predict):
+    return ae(test, predict).mean()
 
 
 # Squared Error
-def se(y_test, y_predict):
-    return (y_test - y_predict) ** 2
+def se(test, predict):
+    return (test - predict) ** 2
+
+
+# Mean Squared Error
+def mse(test, predict):
+    return se(test, predict).mean()
 
 
 # Root Mean Squared Error
-def rmse(y_test, y_predict):
-    return np.sqrt(se(y_test, y_predict).mean())
+def rmse(test, predict):
+    return math.sqrt(mse(test, predict))
 
 
 data = pd.read_csv("energydata_complete.csv", header=0)
@@ -36,31 +43,37 @@ data['time_int'] = time_list
 del data['date']
 del data['rv1']
 del data['rv2']
-print(data.columns)
 train_length = int(0.75 * len(data))
-print(train_length)
 
-x_train_all = data.iloc[:train_length, 1:27]
+x_train = data.iloc[:train_length, 1:27]
 y_train = data[:train_length]['Appliances']
-x_test_all = data.iloc[train_length:, 1:27]
+x_test = data.iloc[train_length:, 1:27]
 y_test = data[train_length:]['Appliances']
 
 lr = LinearRegression()
-lr.fit(x_train_all, y_train)
+lr.fit(x_train, y_train)
 lmtrain_all = (lr.coef_, lr.intercept_)
 
-y_predict = lr.predict(x_test_all)
-mae = mae(y_test, y_predict)
-print('MAE of all = ', mae)
-rmse = rmse(y_test, y_predict)
-print('RMSE of all = ', rmse)
+y_train_predict = lr.predict(x_train)
+t_mae = mae(y_train, y_train_predict)
+t_rmse = rmse(y_train, y_train_predict)
 
-plt.figure(figsize=(20,8))
-ax = plt.subplot(111)
+y_predict = lr.predict(x_test)
+print(type(y_predict[-1]))
+mae = mae(y_test, y_predict)
+rmse = rmse(y_test, y_predict)
+print('Train:')
+print('MAE of all: ', t_mae)
+print('RMSE of all: ', t_rmse)
+print('Test:')
+print('Mae of all: ', mae)
+print('RMSE of all: ', rmse)
+
+plt.figure(figsize=(20, 8))
+ax1 = plt.subplot(111)
 plt.plot(range(len(y_predict)), y_predict, 'b', label='predict', linewidth=0.8)
 plt.plot(range(len(y_predict)), y_test, 'r', label='test', linewidth=0.5)
 plt.legend(loc='upper right')
-ax.set_title('ROC(all)')
-plt.xlabel('index')
+ax1.set_title('ROC(all variables)')
 plt.ylabel('Appliances')
 plt.show()
