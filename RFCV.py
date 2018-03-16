1f245d87ebdac2d02a800e05ece319a58125bdcc
Lin Bin <linbin@8afbcfa1.cs.st-andrews.ac.uk>
2018-03-16 14:06:45 +0000

    apply cross validation for RF

---
+++ RFCV.py
@@ -0,0 +1,63 @@
+import Functions
+import numpy as np
+import pandas as pd
+import datetime
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import cross_validation
+
+# Read data
+data = pd.read_csv("energydata_complete.csv", header=0)
+time_list = []
+nsm_list = []
+# Process date column
+for date in data['date']:
+    date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    nsm = int(date.hour) * 3600 + int(date.minute) * 60 + int(date.second) * 1
+    date = date.hour
+    time_list.append(date)
+    nsm_list.append(nsm)
+
+data['hour'] = time_list
+data['NSM'] = nsm_list
+# Delete useless data
+del data['date']
+del data['rv1']
+del data['rv2']
+del data['RH_6']
+del data['hour']
+
+# Get training set and testing set
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(data.iloc[:, 1:], data['Appliances'],
+                                                                     random_state=1)
+
+rf = RandomForestRegressor(n_estimators=300, random_state=1)
+t_mses = - cross_validation.cross_val_score(rf, x_train, y_train,
+                                          cv=10, scoring='neg_mean_squared_error')
+t_mae = - cross_validation.cross_val_score(rf, x_train, y_train,
+                                         cv=10, scoring='neg_mean_absolute_error')
+
+# Calculate mean
+t_rmses = []
+for mse in t_mses:
+    mse = np.sqrt(mse)
+    t_rmses.append(mse)
+
+print("Random Forest")
+print("Mean result after 10-fold validation")
+print("MAE: ", t_mae.mean())
+print("RMSE: ", Functions.mean(t_rmses))
+
+# Cross validation for testing set
+test_mses = - cross_validation.cross_val_score(rf, x_test, y_test,
+                                               cv=10, scoring='neg_mean_squared_error')
+test_mae = - cross_validation.cross_val_score(rf, x_test, y_test,
+                                              cv=10, scoring='neg_mean_absolute_error')
+
+# Calculate mean
+test_rmses = []
+for mse in test_mses:
+    mse = np.sqrt(mse)
+    test_rmses.append(mse)
+print("Mean result after 10-fold validation for testing set")
+print("MAE: ", test_mae.mean())
+print("RMSE: ", Functions.mean(test_rmses))
